d940f9c104b1304e9d0f1100ee96477df5e6f816
Durga Prasad <saikamireddi3@gmail.com>
2025-05-21 11:26:03 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,83 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useRef } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const IntialData = [
+    {
+      id: 1,
+      name: "sai",
+      age: 22,
+    },
+    {
+      id: 2,
+      name: "durga",
+      age: 23,
+    },
+    {
+      id: 3,
+      name: "prasad",
+      age: 21,
+    },
+    {
+      id: 4,
+      name: "sathish",
+      age: 26,
+    },
+  ];
+  const intialRef = useRef({});
+  const handleChange = (id, field, value) => {
+    if (!intialRef.current[id]) {
+      intialRef.current[id] = { id: id };
+    }
+    intialRef.current[id][field] = value;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const editedRows = Object.values(intialRef.current);
+    console.log(editedRows);
+    intialRef.current = {};
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Age</td>
+            </tr>
+          </thead>
+          <tbody>
+            {IntialData.map((row) => (
+              <tr key={row.id}>
+                <td>
+                  <input
+                    type="text"
+                    defaultValue={row.name}
+                    onChange={(e) => {
+                      handleChange(row.id, "name", e.target.value);
+                    }}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    defaultValue={row.age}
+                    onChange={(e) => {
+                      handleChange(row.id, "age", e.target.value);
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <br />
+        <button type="submit">Save Changes</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
